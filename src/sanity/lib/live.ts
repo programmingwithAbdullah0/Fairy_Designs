3b41de571d8d33cf13b9eb2fb99ffc82c0f52cf8
Abdullah <abdullahateeq331@gmail.com>
2025-10-28 17:22:38 +0500

    FD03

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,9 +1,15 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity/live";
+// import { defineLive } from "next-sanity/live";
+// import { client } from './client'
+
+// export const { sanityFetch, SanityLive } = defineLive({
+//   client,
+// });
+
 import { client } from './client'
 
-export const { sanityFetch, SanityLive } = defineLive({
-  client,
-});
+// ✅ Temporary fix - live feature disable
+export const sanityFetch = client.fetch
+export const SanityLive = () => null
